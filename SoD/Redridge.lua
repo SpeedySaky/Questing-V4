cc393a88815330dfe9ff51c152b4f501c57e1ce7
SpeedySaky <speedysaky@gmail.com>
2024-01-17 15:08:40 +0100

    Create Redridge.lua

---
+++ SoD/Redridge.lua
@@ -0,0 +1,128 @@
+StartMobAvoidance();
+UseDBToRepair(true)
+UseDBToSell(true)
+SetQuestRepairAt(30)
+SetQuestSellAt(2)
+Player = GetPlayer();
+
+AddNameToAvoidWhiteList("Defias Trapper");
+AddNameToAvoidWhiteList("Defias Smuggler");
+
+Log("Going to SW");
+
+TurnInQuestUsingDB(135);
+
+Log("Going to SW");
+TurnInQuestUsingDB(141);
+
+
+CompleteObjectiveOfQuest(142,1)
+TurnInQuestUsingDB(142);
+
+
+Log("Going to Redridge");
+
+TurnInQuestUsingDB(119);
+TurnInQuestUsingDB(121);
+TurnInQuestUsingDB(130);
+
+AcceptQuestUsingDB(92);
+
+AcceptQuestUsingDB(3741);
+AcceptQuestUsingDB(125);
+CompleteObjectiveOfQuest(3741,1)
+
+
+CompleteObjectiveOfQuest(125,1)
+
+TurnInQuestUsingDB(3741);
+TurnInQuestUsingDB(125);
+TurnInQuestUsingDB(143);
+TurnInQuestUsingDB(144);
+TurnInQuestUsingDB(131);
+AcceptQuestUsingDB(122);
+
+Log("Doing Reddridge Goulash");
+
+
+
+
+Boar = {}; 
+Boar[1] = 547;  
+KillLootBoar = CreateObjective("KillMobsAndLoot",1,5,3,92,TableToList(Boar));
+MyInfo = DoObjective(KillLootBoar);
+
+Condor = {}; 
+Condor[1] = 428;  
+KillLootCondor = CreateObjective("KillMobsAndLoot",2,5,3,92,TableToList(Condor));
+MyInfo = DoObjective(KillLootCondor);
+
+Spider = {}; 
+Spider[1] = 442;  
+KillLootSpider = CreateObjective("KillMobsAndLoot",3,5,3,92,TableToList(Spider));
+MyInfo = DoObjective(KillLootSpider);
+
+GrindAreaUntilLevel(21)
+GrindAreaUntilLevel(21)
+GrindAreaUntilLevel(21)
+
+
+
+TurnInQuestUsingDB(92);
+TurnInQuestUsingDB(122);
+
+CompleteObjectiveOfQuest(246,1)
+CompleteObjectiveOfQuest(246,2)
+
+TurnInQuestUsingDB(246);
+
+CompleteObjectiveOfQuest(124,1)
+CompleteObjectiveOfQuest(124,2)
+
+CompleteObjectiveOfQuest(89,1)
+CompleteObjectiveOfQuest(89,2)
+
+
+
+TurnInQuestUsingDB(89);
+TurnInQuestUsingDB(124);
+TurnInQuestUsingDB(145);
+AcceptQuestUsingDB(144);
+AcceptQuestUsingDB(164);
+AcceptQuestUsingDB(245);
+AcceptQuestUsingDB(5);
+CompleteObjectiveOfQuest(245,1)
+TurnInQuestUsingDB(165);
+AcceptQuestUsingDB(148);
+
+TurnInQuestUsingDB(164);
+GrindAreaUntilLevel(22);
+AcceptQuestUsingDB(226);
+
+AcceptQuestFrom(311,95); 
+TurnInQuestUsingDB(165);
+AcceptQuestUsingDB(148);
+CompleteObjectiveOfQuest(226,1)
+CompleteObjectiveOfQuest(226,2)
+TurnInQuestUsingDB(226);
+TurnInQuestUsingDB(245);
+TurnInQuestUsingDB(5);
+TurnInQuestUsingDB(148);
+AcceptQuestUsingDB(94);
+AcceptQuestUsingDB(20);
+AcceptQuestUsingDB(150);
+AcceptQuestUsingDB(127);
+AcceptQuestUsingDB(91);
+TurnInQuestUsingDB(146);
+AcceptQuestUsingDB(128);
+CompleteObjectiveOfQuest(150,1)
+CompleteObjectiveOfQuest(127,1)
+CompleteObjectiveOfQuest(20,1)
+TurnInQuestUsingDB(150);
+TurnInQuestUsingDB(127);
+TurnInQuestUsingDB(20);
+
+
+Log("This is the end of Redridge questing profile");
+
+StopQuestProfile();
